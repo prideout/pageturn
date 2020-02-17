8aa7b254d7f32da91875ff2ae59a1eed195bf713
Philip Rideout <philiprideout@gmail.com>
2020-02-17 08:57:12 -0800

    First commit.

---
+++ materials/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+FILAMENT_REPO=~/github/filament
+FILAMENT_OUT=$FILAMENT_REPO/out/cmake-release
+MATC=$FILAMENT_OUT/tools/matc/matc
+MATINFO=$FILAMENT_OUT/tools/matinfo/matinfo
+MATC_FLAGS='-a opengl -p mobile'
+
+$MATC $MATC_FLAGS -o pbr.filamat pbr.mat
+echo $MATINFO -g 1 pbr.filamat
+
+$MATC $MATC_FLAGS -o nonlit.filamat nonlit.mat
+echo $MATINFO -g 1 nonlit.filamat
+
+$MATC $MATC_FLAGS -o tex.filamat tex.mat
+echo $MATINFO -g 1 tex.filamat
---
+++ update_filament.sh
@@ -0,0 +1,4 @@
+cp ../filament/out/cmake-webgl-release/web/filament-js/filament.js ../pageturn_source/filament/
+cp ../filament/out/cmake-webgl-release/web/filament-js/filament.d.ts ../pageturn_source/filament/
+cp ../filament/out/cmake-webgl-release/web/filament-js/filament.wasm ../pageturn_source/filament/
+cp ../filament/out/cmake-webgl-release/web/filament-js/filament.wasm .
